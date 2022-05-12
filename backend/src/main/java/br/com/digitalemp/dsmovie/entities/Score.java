d2162691bec4e0cdf5b12e2e26acb613ded453c1
Marcio Balian <marciobalian@gmail.com>
2022-05-12 15:19:01 -0300

    Domain model, database seed

---
+++ backend/src/main/java/br/com/digitalemp/dsmovie/entities/Score.java
@@ -0,0 +1,51 @@
+package br.com.digitalemp.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+
+	private Double value;
+
+	public Score() {
+	}
+
+	public void setMovie(Movie movie) {
+		this.id.setMovie(movie);
+	}
+
+	public void setUser(User user) {
+		this.id.setUser(user);
+	}
+
+	public Movie getMovie() {
+		return this.id.getMovie();
+	}
+	
+	public User getUser() {
+		return this.id.getUser();
+	}
+	
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+}
